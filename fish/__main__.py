4384461281926127222cf3e1354eb1c960ef19a7
dave <dbucki01@tufts.edu>
2017-08-30 17:36:05 -0400

    fixed file loading bug, updated manual (still need new figs)

--- fish/__main__.py
+++ fish/__main__.py
@@ -1,11 +1,11 @@
 import sys
-from fish import am_gui
+from fish import ic_gui
 
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
 
-    am_gui.main()
+    ic_gui.main()
 
 if __name__ == "__main__":
     main()
