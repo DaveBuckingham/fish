9d9f20025535a8ab720dd6c35c3728df8b5c8d54
Dave <david.buckingham@tufts.edu>
2018-03-12 17:22:47 -0400

    removed extra stuff not needed for imucapture program

---
+++ setup_cxfreeze_windows.py
@@ -0,0 +1,30 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(
+    packages = [],
+    excludes = [],
+    includes = ['numpy.core._methods',
+                'numpy.lib.format',
+                'pyqtgraph.debug',
+                'pyqtgraph.ThreadsafeTimer',
+                'timeit',
+               ],
+    include_files = ['imucapture\\VERSION']
+)
+    
+base = 'Console'
+#import sys
+#base = 'Win32GUI' if sys.platform=='win32' else None
+
+executables = [
+    Executable('imucapture\\__main__.py', base=base, targetName = 'imu_capture.exe')
+]
+
+
+setup(name='IMU-Capture',
+      version = '0.1',
+      description = 'Record data from IMU',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
